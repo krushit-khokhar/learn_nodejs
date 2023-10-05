619d08fa5835c10fc7b22be7557e2c27b934808d
krushit khokhar <khokharkrushit123@gmail.com>
2023-10-05 16:43:23 +0530

    basic nodejs ponit

---
+++ 3.nodejsBasics/First/routes.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const {url, method} = req;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/messages' method='POST'><input type='text' name='messages'><button>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/messages" && method === "POST") {
+    const data = [];
+    req.on("data", (chunk) => {
+      data.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(data).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("messages.txt", message, (error) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  // res.setHeader('Content-Type', 'text/html'); // Corrected the header name
+  // res.write("<html>");
+  // res.write("<head><title>My First Pages</title></head>");
+  // res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  // res.write("</html>");
+  res.end();
+};
+
+module.exports = requestHandler;
