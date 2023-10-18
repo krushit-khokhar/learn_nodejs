ef0a8fd21202cb3e74589f3d15cd73ea5f6ec66f
krushit khokhar <khokharkrushit123@gmail.com>
2023-10-18 18:59:41 +0530

    use express route

--- 5_express_js/index.js
+++ 5_express_js/index.js
@@ -2,23 +2,17 @@ const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
 
+
+const adminRoutes = require("./Routes/admin");
+const shopRoutes = require("./Routes/shop");
+
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use("/", (req, res, next)=>{
-    next();
-})
-app.use("/add-product",(req,res,next)=>{
-    res.send("<form action='/product' method='POST'><input type='text' name='title'/><button type='submit'>Add Product</button></form>")
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.post("/product",(req,res,next)=>{
-    const {body} = req;
-    console.log(body)
-    res.redirect("/");
-})
-app.use("/", (req, res, next)=>{
-    res.send("<h1>Hello Express Js!</h1>")
-})
 
 
 app.listen(3030)
