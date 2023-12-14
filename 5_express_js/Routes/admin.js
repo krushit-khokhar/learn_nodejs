d4613ddefd0dd0b7890853e4df2eb2305b1b7610
krushit khokhar <khokharkrushit123@gmail.com>
2023-12-14 14:44:16 +0530

    use pug Templating

--- 5_express_js/Routes/admin.js
+++ 5_express_js/Routes/admin.js
@@ -1,16 +1,21 @@
 const path = require('path');
 const rootDir = require('../util/path');
 const express = require('express');
+
 const router = express.Router();
+const products = [];
 
 router.get("/product",(req,res,next)=>{
-    res.sendFile(path.join(rootDir,"views","product.html"))
+   res.render('product',{pageTitle:"Add Product", path:"/admin/product"})
 })
 
 router.post("/product",(req,res,next)=>{
-    const {body} = req;
-    console.log(body)
+if(req.body.title){
+
+    products.push({title:req.body.title})
     res.redirect("/");
+}
 })
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- 5_express_js/Routes/shop.js
+++ 5_express_js/Routes/shop.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
+const express = require("express");
+const path = require("path");
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 const router = express.Router();
-
-router.get("/", (req, res, next)=>{
-    res.sendFile(path.join(rootDir,'views','shop.html'))
-})
+router.get("/", (req, res, next) => {
+  const products = adminData.products;
+  console.log(products)
+  res.render("shop",{prods:products, pageTitle:"Shop", path:"/"});
+});
 
 module.exports = router;
